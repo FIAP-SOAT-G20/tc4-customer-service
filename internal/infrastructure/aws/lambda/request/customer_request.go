074d2b55f2a551655a66a5a55c938a57c5363d76
Marcos Santos <89700349+marcos-nsantos@users.noreply.github.com>
2025-07-18 18:42:20 -0500

    feat: enhance customer request handling and add JSON presenter

--- internal/infrastructure/aws/lambda/request/customer_request.go
+++ internal/infrastructure/aws/lambda/request/customer_request.go
@@ -3,11 +3,30 @@ package request
 import "github.com/FIAP-SOAT-G20/tc4-customer-service/internal/core/dto"
 
 type CustomerRequest struct {
-	CPF string `json:"cpf"`
+	ID    string `json:"id,omitempty"`
+	Name  string `json:"name,omitempty"`
+	Email string `json:"email,omitempty"`
+	CPF   string `json:"cpf,omitempty"`
 }
 
 func (c CustomerRequest) ToGetCustomerInput() dto.GetCustomerInput {
 	return dto.GetCustomerInput{
-		CPF: c.CPF,
+		ID: c.ID,
+	}
+}
+
+func (c CustomerRequest) ToCreateCustomerInput() dto.CreateCustomerInput {
+	return dto.CreateCustomerInput{
+		Name:  c.Name,
+		Email: c.Email,
+		CPF:   c.CPF,
+	}
+}
+
+func (c CustomerRequest) ToUpdateCustomerInput() dto.UpdateCustomerInput {
+	return dto.UpdateCustomerInput{
+		ID:    c.ID,
+		Name:  c.Name,
+		Email: c.Email,
 	}
 }
