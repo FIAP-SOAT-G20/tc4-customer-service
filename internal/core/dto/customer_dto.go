78767b9517d933eecb77d7c6a6604b5502cde08e
Marcos Santos <89700349+marcos-nsantos@users.noreply.github.com>
2025-07-18 17:32:05 -0500

    feat: implement customer management operations
    - Added CRUD operations (`Create`, `Read`, `Update`, `Delete`, `List`) in `CustomerUseCase`
    - Introduced corresponding DTOs and updated domain entity for customers
    - Extended interface definitions in `customer_port.go` to include new operations
    - Created new mocks for `CustomerGateway`, `CustomerUseCase`, and related interfaces
    - Replaced test fixture with suite-based testing for `CustomerUseCase`
    - Updated tests to validate `CustomerUseCase` functionality

--- internal/core/dto/customer_dto.go
+++ internal/core/dto/customer_dto.go
@@ -1,9 +1,45 @@
 package dto
 
+import "github.com/FIAP-SOAT-G20/tc4-customer-service/internal/core/domain/entity"
+
+type CreateCustomerInput struct {
+	Name  string
+	Email string
+	CPF   string
+}
+
+func (i CreateCustomerInput) ToEntity() *entity.Customer {
+	return &entity.Customer{
+		Name:  i.Name,
+		Email: i.Email,
+		CPF:   i.CPF,
+	}
+}
+
+type UpdateCustomerInput struct {
+	ID    string
+	Name  string
+	Email string
+}
+
 type GetCustomerInput struct {
+	ID string
+}
+
+type GetCustomerByCPFInput struct {
 	CPF string
 }
 
-type CustomerDatasourceFilter struct {
-	CPF *string
+type DeleteCustomerInput struct {
+	ID string
+}
+
+type ListCustomersInput struct {
+	Name  string
+	Page  int
+	Limit int
+}
+
+type FindCustomerByCPFInput struct {
+	CPF string
 }
--- internal/core/port/customer_port.go
+++ internal/core/port/customer_port.go
@@ -8,17 +8,36 @@ import (
 )
 
 type CustomerController interface {
+	List(ctx context.Context, presenter Presenter, input dto.ListCustomersInput) ([]byte, error)
+	Create(ctx context.Context, presenter Presenter, input dto.CreateCustomerInput) ([]byte, error)
 	Get(ctx context.Context, presenter Presenter, input dto.GetCustomerInput) ([]byte, error)
+	Update(ctx context.Context, presenter Presenter, input dto.UpdateCustomerInput) ([]byte, error)
+	Delete(ctx context.Context, presenter Presenter, input dto.DeleteCustomerInput) ([]byte, error)
 }
 
 type CustomerUseCase interface {
+	List(ctx context.Context, input dto.ListCustomersInput) ([]*entity.Customer, int64, error)
+	Create(ctx context.Context, input dto.CreateCustomerInput) (*entity.Customer, error)
 	Get(ctx context.Context, input dto.GetCustomerInput) (*entity.Customer, error)
+	GetByCPF(ctx context.Context, i dto.GetCustomerByCPFInput) (*entity.Customer, error)
+	Update(ctx context.Context, input dto.UpdateCustomerInput) (*entity.Customer, error)
+	Delete(ctx context.Context, input dto.DeleteCustomerInput) (*entity.Customer, error)
 }
 
 type CustomerGateway interface {
-	FindOne(ctx context.Context, cpf string) (*entity.Customer, error)
+	FindByID(ctx context.Context, id string) (*entity.Customer, error)
+	FindByCPF(ctx context.Context, cpf string) (*entity.Customer, error)
+	FindAll(ctx context.Context, name string, page, limit int) ([]*entity.Customer, int64, error)
+	Create(ctx context.Context, customer *entity.Customer) error
+	Update(ctx context.Context, customer *entity.Customer) error
+	Delete(ctx context.Context, id string) error
 }
 
 type CustomerDataSource interface {
-	FindOne(ctx context.Context, filters dto.CustomerDatasourceFilter) (*entity.Customer, error)
+	FindByID(ctx context.Context, id string) (*entity.Customer, error)
+	FindByCPF(ctx context.Context, cpf string) (*entity.Customer, error)
+	FindAll(ctx context.Context, filters map[string]interface{}, page, limit int) ([]*entity.Customer, int64, error)
+	Create(ctx context.Context, product *entity.Customer) error
+	Update(ctx context.Context, product *entity.Customer) error
+	Delete(ctx context.Context, id string) error
 }
